eca3ce270f0b7aae197d495451c35434c761aeb7
mdfsar15 <57721986+mdafsar15@users.noreply.github.com>
2020-09-24 22:32:05 +0530

    [AFSAR]ADDED: Create a Fusion Web Application, Build the Business Services, Develop a JavaServer Faces page, Test the page from a browser, modify the layout of the page

---
+++ ViewController/src/genx/view/backing/HR.java
@@ -0,0 +1,170 @@
+package genx.view.backing;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.data.RichListItem;
+import oracle.adf.view.rich.component.rich.data.RichListView;
+import oracle.adf.view.rich.component.rich.fragment.RichPageTemplate;
+import oracle.adf.view.rich.component.rich.layout.RichGridCell;
+import oracle.adf.view.rich.component.rich.layout.RichGridRow;
+import oracle.adf.view.rich.component.rich.layout.RichPanelBox;
+import oracle.adf.view.rich.component.rich.layout.RichPanelDashboard;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGridLayout;
+import oracle.adf.view.rich.component.rich.output.RichMessages;
+import oracle.adf.view.rich.component.rich.output.RichOutputFormatted;
+
+public class HR {
+    private RichPageTemplate pt1;
+    private RichForm f1;
+    private RichDocument d1;
+    private RichListView lv1;
+    private RichListItem li1;
+    private RichPanelGridLayout pgl1;
+    private RichGridRow gr1;
+    private RichGridCell gc1;
+    private RichOutputFormatted of1;
+    private RichGridCell gc2;
+    private RichOutputFormatted of2;
+    private RichMessages m1;
+    private RichPanelDashboard pd1;
+    private RichPanelBox pb1;
+    private RichPanelBox pb2;
+    private RichPanelBox pb3;
+    private RichPanelBox pb4;
+
+    public void setPt1(RichPageTemplate pt1) {
+        this.pt1 = pt1;
+    }
+
+    public RichPageTemplate getPt1() {
+        return pt1;
+    }
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+
+    public void setLv1(RichListView lv1) {
+        this.lv1 = lv1;
+    }
+
+    public RichListView getLv1() {
+        return lv1;
+    }
+
+    public void setLi1(RichListItem li1) {
+        this.li1 = li1;
+    }
+
+    public RichListItem getLi1() {
+        return li1;
+    }
+
+    public void setPgl1(RichPanelGridLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGridLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setGr1(RichGridRow gr1) {
+        this.gr1 = gr1;
+    }
+
+    public RichGridRow getGr1() {
+        return gr1;
+    }
+
+    public void setGc1(RichGridCell gc1) {
+        this.gc1 = gc1;
+    }
+
+    public RichGridCell getGc1() {
+        return gc1;
+    }
+
+    public void setOf1(RichOutputFormatted of1) {
+        this.of1 = of1;
+    }
+
+    public RichOutputFormatted getOf1() {
+        return of1;
+    }
+
+    public void setGc2(RichGridCell gc2) {
+        this.gc2 = gc2;
+    }
+
+    public RichGridCell getGc2() {
+        return gc2;
+    }
+
+    public void setOf2(RichOutputFormatted of2) {
+        this.of2 = of2;
+    }
+
+    public RichOutputFormatted getOf2() {
+        return of2;
+    }
+
+    public void setM1(RichMessages m1) {
+        this.m1 = m1;
+    }
+
+    public RichMessages getM1() {
+        return m1;
+    }
+
+    public void setPd1(RichPanelDashboard pd1) {
+        this.pd1 = pd1;
+    }
+
+    public RichPanelDashboard getPd1() {
+        return pd1;
+    }
+
+    public void setPb1(RichPanelBox pb1) {
+        this.pb1 = pb1;
+    }
+
+    public RichPanelBox getPb1() {
+        return pb1;
+    }
+
+    public void setPb2(RichPanelBox pb2) {
+        this.pb2 = pb2;
+    }
+
+    public RichPanelBox getPb2() {
+        return pb2;
+    }
+
+    public void setPb3(RichPanelBox pb3) {
+        this.pb3 = pb3;
+    }
+
+    public RichPanelBox getPb3() {
+        return pb3;
+    }
+
+    public void setPb4(RichPanelBox pb4) {
+        this.pb4 = pb4;
+    }
+
+    public RichPanelBox getPb4() {
+        return pb4;
+    }
+}
